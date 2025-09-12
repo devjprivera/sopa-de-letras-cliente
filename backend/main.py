eec6f1528af4d8ab4013899e87ce5cc2f19592b8
Juan Pablo Rivera Sierra <dev.jprivera@gmail.com>
2025-09-12 15:51:46 -0500

    Created base index for form and imported it in app.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
-from soup import soupSolver
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from typing import Optional
+from os import path
+
+from soup import soupSolver
 
 app = FastAPI()
+
+BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+TEMPLATES_DIR = path.join(BASE_DIR, "frontend", "templates")
+
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 # Model for alphabet soup
 class Soup(BaseModel):
@@ -12,9 +21,10 @@ class Soup(BaseModel):
     size: Optional[int] = 14
 
 # Shows Main Page
-@app.get("/")
-def main():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def main(request: Request):
+    # return {"Hello": "World"}
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/solver")
 def solver(soup: Soup):
