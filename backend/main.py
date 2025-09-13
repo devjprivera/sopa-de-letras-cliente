55588b8621ff0aad134e8eae4ca3a79b19ac5039
Juan Pablo Rivera Sierra <dev.jprivera@gmail.com>
2025-09-12 23:10:37 -0500

    Added form for alphabet soup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import Optional
 from os import path
@@ -10,25 +11,26 @@ from soup import soupSolver
 app = FastAPI()
 
 BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
-TEMPLATES_DIR = path.join(BASE_DIR, "frontend", "templates")
 
+TEMPLATES_DIR = path.join(BASE_DIR, "frontend", "templates")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
-# Model for alphabet soup
-class Soup(BaseModel):
-    soup: str
-    words: str
-    size: Optional[int] = 14
+STATIC_DIR = path.join(BASE_DIR, "frontend", "static")
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 # Shows Main Page
 @app.get("/", response_class=HTMLResponse)
 def main(request: Request):
-    # return {"Hello": "World"}
+
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/solver")
-def solver(soup: Soup):
+@app.post("/")
+def solver(request: Request, words: str = Form(...), soup: str = Form(...)):
 
-    wordsFound = soupSolver(soup.soup, soup.words, soup.size)
+    # wordsFound = soupSolver(soup.soup, soup.words, soup.size)
+    # wordsFound = {
+    #     "words": soup.words,
+    #     "soup": soup.soup,
+    # }
 
-    return wordsFound
+    return {"words": words, "soup": soup}
